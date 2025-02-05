f3a7e3d81ed6c52ecfdf853bc0f016a8c4ef3fe9
Max <nexusitproger@gmail.com>
2025-02-05 21:02:13 +0500

    GH pages bug fix

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,6 +1,5 @@
 import "./Main.style.scss";
 import { Hero } from "../../components/Hero/Hero";
-import { type Slide } from "../../types/heroSlider.types";
 import { PrimaryProducts } from "../../components/PrimaryProducts/PrimaryProducts";
 import { exampleProducts } from "../../data/exampleProducts";
 import { OurCollection } from '../../components/OurCollection/OurCollection';
@@ -9,34 +8,34 @@ import { ShortAbout } from '../../components/ShortAbout/ShortAbout';
 import { FAQ } from '../../components/FAQ/FAQ';
 import { Subscribe } from '../../components/Subscribe/Subscribe';
 
-const slides: Slide[] = [
-  {
-    id: 1,
-    title: "Летняя коллекция",
-    description: "Откройте для себя яркие краски лета",
-    image: "",
-    link: "/collections/summer",
-  },
-  {
-    id: 2,
-    title: "Осенний стиль",
-    description: "Теплые оттенки для прохладных дней",
-    image: "",
-    link: "/collections/autumn",
-  },
-  {
-    id: 3,
-    title: "Зимний гардероб",
-    description: "Элегантность в каждой детали",
-    image: "",
-    link: "/collections/winter",
-  },
-];
+// const slides: Slide[] = [
+//   {
+//     id: 1,
+//     title: "Летняя коллекция",
+//     description: "Откройте для себя яркие краски лета",
+//     image: "",
+//     link: "/collections/summer",
+//   },
+//   {
+//     id: 2,
+//     title: "Осенний стиль",
+//     description: "Теплые оттенки для прохладных дней",
+//     image: "",
+//     link: "/collections/autumn",
+//   },
+//   {
+//     id: 3,
+//     title: "Зимний гардероб",
+//     description: "Элегантность в каждой детали",
+//     image: "",
+//     link: "/collections/winter",
+//   },
+// ];
 
 export const Main = () => {
   return (
     <main className="main">
-      <Hero slides={slides} />
+      <Hero />
       <PrimaryProducts products={exampleProducts} />
       <OurCollection />
       <LatestCollection 
